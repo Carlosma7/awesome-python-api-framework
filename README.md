231d9dbe4a5772c6e398bb1f35061f7dc2309474
Carlosma7 <carlos7ma@gmail.com>
2022-12-11 03:01:07 +0100

    Close #2 Upload FastAPI

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 A curated collection of awesome frameworks related to API definition in Python.
 
 <a href="https://carlosma7.github.io/awesome-python-api-framework/doc/flask" target="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1280px-Flask_logo.svg.png" width="100" alt="Flask"></img></a> [Flask](https://carlosma7.github.io/awesome-python-api-framework/doc/flask)
+
+<a href="https://carlosma7.github.io/awesome-python-api-framework/doc/fastapi" target="_blank"><img src="https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png" width="100" alt="FastAPI"></img></a> [FastAPI](https://carlosma7.github.io/awesome-python-api-framework/doc/fastapi)
---
+++ src/fastapi_example.py
@@ -0,0 +1,47 @@
+"""FastAPI program example to show a simple configuration."""
+
+import logging
+from fastapi import FastAPI
+
+# Define flask app
+app = FastAPI()
+
+# Define level of logging and logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Just for testing
+global names
+names = []
+
+# GET method: curl http://localhost:8080/names
+@app.get('/names')
+def get_names():
+    """GET method that returns list of names."""
+    return names
+
+
+# POST method: curl -X POST -H "content-Type: application/json"
+# -d '{"name": "Carlos"}' http://localhost:8080/add
+@app.post('/add')
+def add_name(item: dict):
+    """POST method that adds a new name into list."""
+    name = item.get('name')
+
+    logger.info("Add new name '%s'", name)
+    names.append(name)
+
+    return 'Name added'
+
+
+# DELETE method: curl -X DELETE -H "content-Type: application/json"
+# -d '{"name": "Carlos"}' http://localhost:8080/remove
+@app.delete('/remove')
+def remove_name(item: dict):
+    """DELETE method that removes a name from the list."""
+    name = item.get('name')
+
+    logger.info("Remove name '%s'", name)
+    names.remove(name)
+
+    return 'Name removed'
